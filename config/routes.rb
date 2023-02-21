a79fa64383c1651b3322915a3819a0170443491b
robin vorstedt robinvorstedt@outlook.com <robinvorstedt@outlook.com>
2023-02-21 15:40:04 +0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "task/create", to: "tasks#new"
+  get "tasks", to: "tasks#index"
+
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+  # get "show", to: "show/"
 end
